607ce5393ccaf12748c683c344a6158cf02a69f2
darkobas <marko@datafund.io>
2024-10-01 16:58:13 +0200

    v2

--- run.sh
+++ run.sh
@@ -4,13 +4,15 @@ LOGFILE="logfile.log"
 
 {
   echo "Starting script execution at $(date)"
+  python3 app.py --upload --repeat 1 --size 1
   python3 app.py --upload --repeat 2 --size 1
-  python3 app.py --upload --repeat 2 --size 10
-  python3 app.py --upload --repeat 2 --size 100
-  python3 app.py --upload --repeat 2 --size 1000
-  python3 app.py --upload --repeat 2 --size 10000
-  python3 app.py --upload --repeat 2 --size 50000
-  python3 app.py --upload --repeat 2 --size 100000
-  python3 app.py --download --repeat 3
-  python3 app.py --gateway --repeat 3
+  #python3 app.py --upload --repeat 2 --size 10
+  #python3 app.py --upload --repeat 2 --size 100
+  #python3 app.py --upload --repeat 2 --size 1000
+  #python3 app.py --upload --repeat 2 --size 10000
+  #python3 app.py --upload --repeat 2 --size 50000
+  #python3 app.py --upload --repeat 2 --size 100000
+  #python3 app.py --download --repeat 3
+  #python3 app.py --gateway --repeat 3
+  echo "all runs completed $(date)"
 } 2>&1 | tee -a "$LOGFILE"
