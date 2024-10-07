b4d50f965a0a075ce484aa668766259c3c5d8ae5
darkobas <marko@datafund.io>
2024-10-08 00:04:48 +0200

    update config and run script

--- run.sh
+++ run.sh
@@ -5,14 +5,12 @@ LOGFILE="logfile.log"
 {
   echo "Starting script execution at $(date)"
   python3 app.py --upload --repeat 1 --size 1
-  python3 app.py --upload --repeat 2 --size 1
-  #python3 app.py --upload --repeat 2 --size 10
-  #python3 app.py --upload --repeat 2 --size 100
-  #python3 app.py --upload --repeat 2 --size 1000
-  #python3 app.py --upload --repeat 2 --size 10000
-  #python3 app.py --upload --repeat 2 --size 50000
-  #python3 app.py --upload --repeat 2 --size 100000
-  #python3 app.py --download --repeat 3
-  #python3 app.py --gateway --repeat 3
+  python3 app.py --upload --repeat 1 --size 10
+  python3 app.py --upload --repeat 1 --size 100
+  python3 app.py --upload --repeat 1 --size 1000
+  python3 app.py --upload --repeat 1 --size 10000
+  python3 app.py --upload --repeat 1 --size 50000
+  sleep 600
+  python3 app.py --download --repeat 30
   echo "all runs completed $(date)"
 } 2>&1 | tee -a "$LOGFILE"
