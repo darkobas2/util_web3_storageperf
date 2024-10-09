a99d213a5e881ac89c5b7d59965eb80153861d7a
Gyorgy Barabas <dysordys@umich.edu>
2024-10-09 16:54:43 +0200

    Updated analysis-code/analysis.R. Before, it converted the ip addresses to server numbers based on hard-coded information that I copied from the config.json file. Now the program automatically reads in the config file and parses the servers from there.

--- analysis-code/analysis.R
+++ analysis-code/analysis.R
@@ -1,38 +1,51 @@
 library(tidyverse)
 
 
-dat <-
-  jsonlite::fromJSON("../data/results-no50.json") |>
-  tibble() |>
-  rename(json = 1) |>
-  mutate(storage = c("Swarm", "IPFS", "Arweave"), .before = 1) |>
-  unnest(json) |>
-  unnest(json) |>
-  relocate(size, .after = storage) |>
-  rename(time = download_time_seconds, size_kb = size) |>
-  mutate(server = case_when(
-    ip %in% c("download.gateway.ethswarm.org", "5.9.50.180:8080",
-              "https://permagate.io") ~ "Server 1",
-    ip %in% c("188.245.154.61:1633", "188.245.154.61:8080",
-              "https://ar.perplex.finance") ~ "Server 2",
-    ip %in% c("188.245.177.151:1633", "188.245.177.151:8080",
-              "https://ar-io.dev") ~ "Server 3"
-  )) |>
-  mutate(size_kb = case_when(
-    size_kb ==     "1" ~ "1 KB",
-    size_kb ==    "10" ~ "10 KB",
-    size_kb ==   "100" ~ "100 KB",
-    size_kb ==  "1000" ~ "1 MB",
-    size_kb == "10000" ~ "10 MB"
-  )) |>
-  mutate(size_kb = fct_relevel(size_kb, "1 KB", "10 KB", "100 KB", "1 MB", "10 MB"))
+serversFromConfig <- function(configFile = "../config.json") {
+  jsonlite::fromJSON(configFile) |>
+    as_tibble() |>
+    select(contains("dl")) |>
+    mutate(server = str_c("Server ", 1:3), .before = 1) |>
+    rename_with(\(x) str_remove(x, "_dl_servers"), !server) |>
+    pivot_longer(!server, names_to = "storage", values_to = "ip") |>
+    mutate(storage = case_match(
+      storage,
+      "swarm" ~ "Swarm",
+      "ipfs"  ~ "IPFS",
+      "arw"   ~ "Arweave"
+    ))
+}
 
 
-dat |>
+dataFromJson <- function(jsonFile = "../data/results-no50.json") {
+  jsonlite::fromJSON(jsonFile) |>
+    tibble() |>
+    rename(json = 1) |>
+    mutate(storage = c("Swarm", "IPFS", "Arweave"), .before = 1) |>
+    unnest(json) |>
+    unnest(json) |>
+    rename(time_sec = download_time_seconds) |>
+    select(!server) |>
+    left_join(serversFromConfig(), by = join_by(storage, ip)) |>
+    mutate(size_kb = case_when(
+      size ==     "1" ~ "1 KB",
+      size ==    "10" ~ "10 KB",
+      size ==   "100" ~ "100 KB",
+      size ==  "1000" ~ "1 MB",
+      size == "10000" ~ "10 MB"
+    )) |>
+    mutate(size_kb = fct_reorder(size_kb, as.integer(size))) |>
+    select(!size) |>
+    relocate(size_kb, server, time_sec, attempts, sha256_match, .after = storage)
+}
+
+
+
+dataFromJson() |>
   drop_na() |>
-  select(storage, size_kb, server, time) |>
-  mutate(storage = fct_reorder(storage, time)) |>
-  ggplot(aes(x = time, color = storage, fill = storage)) +
+  select(storage | size_kb | server | time_sec) |>
+  mutate(storage = fct_reorder(storage, time_sec)) |>
+  ggplot(aes(x = time_sec, color = storage, fill = storage)) +
   geom_density(alpha = 0.2) +
   scale_x_continuous() +
   labs(x = "Retrieval time (seconds)", y = "Density",
