60c2a28ddbf13612607e3771cfc993b7afab7efd
darkobas <marko@datafund.io>
2024-10-10 00:55:02 +0200

    first run

--- run.sh
+++ run.sh
@@ -4,13 +4,15 @@ LOGFILE="logfile.log"
 
 {
   echo "Starting script execution at $(date)"
-  python3 app.py --upload --repeat 1 --size 1
-  python3 app.py --upload --repeat 1 --size 10
-  python3 app.py --upload --repeat 1 --size 100
-  python3 app.py --upload --repeat 1 --size 1000
-  python3 app.py --upload --repeat 1 --size 10000
+  python3 app.py --upload --repeat 30 --size 1
+  python3 app.py --upload --repeat 30 --size 10
+  python3 app.py --upload --repeat 30 --size 100
+  python3 app.py --upload --repeat 30 --size 1000
+  python3 app.py --upload --repeat 30 --size 10000
   #python3 app.py --upload --repeat 1 --size 50000
-  sleep 900
-  python3 app.py --download --repeat 30
+  echo "started sleep $(date)"
+  sleep 2h
+  echo "ended sleep $(date)"
+  python3 app.py --download --repeat 1
   echo "all runs completed $(date)"
 } 2>&1 | tee -a "$LOGFILE"
