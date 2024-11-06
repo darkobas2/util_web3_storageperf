1af5f3bdd9531a9d92e28eb4532a4c2dcd663a4a
darkobas <marko@datafund.io>
2024-11-06 21:51:44 +0100

    more runs

--- runswarm.sh
+++ runswarm.sh
@@ -6,37 +6,127 @@ LOGFILE="logfile.log"
   echo "Starting script execution at $(date)"
   python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 0 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 30000ms --dl-redundancy 0 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 0 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10000 --ul-redundancy 0 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 0 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1000 --ul-redundancy 0 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 0 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 100 --ul-redundancy 0 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 0 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10 --ul-redundancy 0 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 0 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1 --ul-redundancy 0 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 0 --only-swarm
   rm references_onlyswarm.json
 
   python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 1 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 1000ms --dl-redundancy 3 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10000 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1000 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 100 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
   rm references_onlyswarm.json
 
   python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 1 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 5000ms --dl-redundancy 3 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10000 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1000 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 100 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1 --ul-redundancy 1 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
   rm references_onlyswarm.json
 
-  python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 1 --only-swarm
+  python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 3 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 30000ms --dl-redundancy 3 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10000 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1000 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 100 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 1 --only-swarm
   rm references_onlyswarm.json
 
-  python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 1 --only-swarm
+  python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 3 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 1000ms --dl-redundancy 1 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
   rm references_onlyswarm.json
-
-  python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 1 --only-swarm
+  python3 app.py --upload --repeat 5 --size 10000 --ul-redundancy 3 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 5000ms --dl-redundancy 1 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
   rm references_onlyswarm.json
-
-  python3 app.py --upload --repeat 5 --size 100000 --ul-redundancy 1 --only-swarm
+  python3 app.py --upload --repeat 5 --size 1000 --ul-redundancy 3 --only-swarm
   sleep 5m
-  python3 app.py --download --repeat 1 --dl-retrieval 30000ms --dl-redundancy 1 --only-swarm
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 100 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 10 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
+  rm references_onlyswarm.json
+  python3 app.py --upload --repeat 5 --size 1 --ul-redundancy 3 --only-swarm
+  sleep 5m
+  python3 app.py --download --repeat 1 --dl-redundancy 3 --only-swarm
   rm references_onlyswarm.json
 
   mv results_onlyswarm.json data/results_onlyswarm_$(date +%F_%H-%M).json
