302f8ff9b2e91e877cb7e2a52a5705a1eb3491d0
Gyorgy Barabas <dysordys@umich.edu>
2025-01-23 16:09:48 +0100

    Analysis of upload times, v1.0

--- analysis-code/upload-times.R
+++ analysis-code/upload-times.R
@@ -2,23 +2,51 @@ library(jsonlite)
 library(tidyverse)
 
 
+
 dataFromJsonRaw <- function(jsonFile) {
   fromJSON(jsonFile) |>
     (`[`)(1) |>
     as_tibble() |>
     unnest(swarm) |>
-    rename(erasure = ul_redundancy) |>
-    mutate(erasure = case_match(
-      erasure,
-      0 ~ "NONE",
-      1 ~ "MEDIUM",
-      2 ~ "STRONG",
-      3 ~ "INSANE",
-      4 ~ "PARANOID",
-      NA ~ "NONE"))
+    rename(erasure = ul_redundancy, time = upload_time)
 }
 
 
-tibble(file = Sys.glob("../references/references_onlyswarm_*_3_2024-12-*.json")) |>
+fileSizeFromJsonRaw <- function(jsonFile) {
+  fromJSON(jsonFile) |>
+    (`[`)(1) |>
+    (`[[`)(1) |>
+    names() |>
+    as.integer()
+}
+
+
+
+dat <-
+  tibble(file = Sys.glob("../references/references_onlyswarm_*2024-12-*.json")) |>
+  mutate(size_kb = map_int(file, fileSizeFromJsonRaw)) |>
   mutate(data = map(file, dataFromJsonRaw)) |>
-  unnest(data)
+  unnest(data) |>
+  select(erasure, size_kb, time) |>
+  arrange(erasure, size_kb, time) |>
+  mutate(erasure = case_match(
+    erasure,
+    0 ~ "NONE",
+    1 ~ "MEDIUM",
+    2 ~ "STRONG",
+    3 ~ "INSANE",
+    4 ~ "PARANOID"
+  ))
+
+
+dat |>
+  mutate(erasure = as_factor(erasure)) |>
+  ggplot(aes(x = as_factor(size_kb), y = time, color = erasure, fill = erasure)) +
+  geom_boxplot(alpha = 0.3, coef = Inf) +
+  scale_x_discrete(labels = c("1 KB", "10 KB", "100 KB", "1 MB", "10 MB", "100 MB")) +
+  scale_y_log10(breaks = 10^(-1:3), labels = c(0.1, 1, 10, 100, 1000)) +
+  scale_color_viridis_d(option = "B", end = 0.85) +
+  scale_fill_viridis_d(option = "B", end = 0.85) +
+  labs(x = "File size", y = "Upload time (seconds)",
+       color = "Erasure coding", fill = "Erasure coding") +
+  theme_bw()
